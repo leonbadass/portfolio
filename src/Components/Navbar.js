5b74f3bb193a18b420975f9f334083200750669d
Godwin Azuka <287015@student.pwr.edu.pl>
2025-05-16 12:11:48 +0200

    update -technicak writing section

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,11 @@
-import React from "react";
+
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between items-center p-4 bg-gray-900 text-white fixed w-full shadow-lg">
       <h1 className="text-1xl font-bold">My Portfolio</h1>
       <ul className="flex gap-6">
+        <li><a href="#documentation" className="bg-gray-200 text-blue-900 font-bold px-2 py-2 rounded-lg hover:bg-gray-700 hover:text-gray-200 hover:text-lg">Technical Writing</a></li>
         <li><a href="#about" className="hover:text-gray-400">About</a></li>
         <li><a href="#projects" className="hover:text-gray-400">Projects</a></li>
         <li><a href="#about" className="hover:text-gray-400">Skills</a></li>
--- src/Components/TechnicalSkills.js
+++ src/Components/TechnicalSkills.js
@@ -15,21 +15,18 @@ function TechnicalSkills() {
   };
 
   return (
-    <section id="documentation"  className="h-screen flex justify-center gap-10 mb-2rem items-center text-center bg-gray-900 text-white px-6 bg-gradient-to-br from-gray-500 via-white-500 to-black-500
-">
-      <h2 className="text-3xl font-bold mb-4">📄 Technical Writing</h2>
-      <p className="text-lg mb-6">
-        Explore detailed technical specs, architecture breakdowns, and design decisions behind my projects.
-      </p>
-      <ul className="space-y-4">
+    <section id="documentation"  className="h-full flex-col flex justify-center mb-2rem items-center text-center bg-gray-900 text-white px-6 py-6 ">
+      <h2 className="text-3xl font-bold mb-4"> Technical Writing</h2>
+     
+      <div className="flex  w-full justify-center gap-10 items-center flex-wrap-reverse text-xl mt-6">
         {Object.entries(projects).map(([key, project]) => (
-          <li key={key}>
-            <a href={project.link} className="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">
+          <h4 key={key}  className="bg-blue-200 py-4 px-4 rounded-lg hover:bg-blue-900  hover:text-blue-200 text-blue-900">
+            <a href={project.link} className=" hover:underline" target="_blank" rel="noopener noreferrer">
               {project.title}
             </a>
-          </li>
+          </h4>
         ))}
-      </ul>
+      </div>
     </section>
   );
 }
